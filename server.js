4c1c380f2ae8e600ee7a3730b97a17deebb3aed5
Enagnon Ulrich  Gislain Segla <97036893+malcomx2022@users.noreply.github.com>
2024-10-01 16:36:40 +0100

    initialisation du projet

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+app.listen(PORT,()=>console.log(`Example app listening on port ${PORT}`))
+app.get('/',function(req,res){
+    res.send('Hello World')
+})
